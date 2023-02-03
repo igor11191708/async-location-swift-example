8809c82fa853ef2e1544b9adec75fa090c00629a
Igor <igorshelopaev@gmail.com>
2023-02-03 12:16:48 +0500

    Initial Commit

---
+++ async-location-swift-example/async_location_swift_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  async_location_swift_exampleApp.swift
+//  async-location-swift-example
+//
+//  Created by Igor on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct async_location_swift_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
